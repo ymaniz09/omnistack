ec461375491d48f3bbfe3d673faeb3022b7b8647
Ygor <ygor.maniz@gmail.com>
2019-06-14 13:06:57 -0300

    Using socket.io for realtime experience

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 
@@ -16,11 +17,30 @@ class Feed extends Component {
     };
 
     async componentDidMount() {
+        this.registerToSocket();
         const response = await api.get('posts');
 
         this.setState({ feed: response.data });
     }
 
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post)
+            });
+        })
+    }
     render() {
         return (
             <section id="post-list">
@@ -37,7 +57,9 @@ class Feed extends Component {
                         <img src={`http://localhost:3333/files/${post.image}`} />
                         <footer>
                             <div className="actions">
-                                <img src={like} alt="Like" />
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="Like" />
+                                </button>
                                 <img src={comment} alt="Comment post" />
                                 <img src={send} alt="Send post to a friend" />
                             </div>
